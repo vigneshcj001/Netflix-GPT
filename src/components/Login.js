301cb35784391d50825238632630efbfda084a01
vigneshcj001 <vigneshcj001@gmail.com>
2024-04-05 11:21:27 +0530

    Commit message

---
+++ src/components/Login.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Header from "./Header";
+
+const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/7ca5b7c7-20aa-42a8-a278-f801b0d65fa1/fb548c0a-8582-43c5-9fba-cd98bf27452f/IN-en-20240326-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img  src={BG_URL} alt="logo" />
+      </div>
+      <form
+        className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+      >
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">Sign In</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
